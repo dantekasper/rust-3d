83fd14fc0157f2c8a81d349d0abeb2fea880aa64
Dante <dkvirtual3@gmail.com>
2022-08-26 18:31:02 -0700

    add webpack and webconfig

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env, args) => {
+    const isProductionMode = (args.mode === 'production');
+
+    return {
+        entry: './index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isProductionMode ? '[name].[contenthash].js' : '[name].[hash].js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new webpack.ProgressPlugin({
+                TextDecoder: ['text-encoding', 'TextDecoder'],
+                TextEncoder: ['text-encoding', 'TextEncoder']
+            })
+        ]
+    }
+}
